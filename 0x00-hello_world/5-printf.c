2554c57e7c43545c6175a77171421f1212d7c091
Salami Ifeoluwa Mistura <omosholapesalami@gmail.com>
2022-07-01 04:18:55 -0700

    Print n excerpt

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: using the main function
+ * this program prints a statement
+ * Return: Always 0(success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+}
