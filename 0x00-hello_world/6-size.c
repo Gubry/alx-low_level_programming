9c3c3100db821fff72c15853e2bc5359baa2b53d
Salami Ifeoluwa Mistura <omosholapesalami@gmail.com>
2022-07-01 05:05:24 -0700

    Print the sizes of types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: using the main function
+ * to print the size of different var type
+ * Return: Always 0(success)
+ */
+
+int main(void)
+{
+	printf("Size of int: %lu byte(s)\n" sizeof(int));
+	printf("Size of char: %lu byte(s)\n" sizeof(char));
+	printf("Size of float: %lu byte(s)\n" sizeof(float));
+	printf("Size of long int: %lu byte(s)\n" sizeof(long int));
+	printf("Size of long long int: %lu byte(s)\n" sizeof(long long int));
+	return (0);
+}
+
