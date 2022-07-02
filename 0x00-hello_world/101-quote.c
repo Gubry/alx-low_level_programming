c302b7693b9746454dbb09b65539950637dedd3c
Salami Ifeoluwa Mistura <omosholapesalami@gmail.com>
2022-07-02 12:23:38 -0700

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry Point
+ * Description: using the main function
+ * to print a quote
+ * Return: Always 1
+ */
+
+int main(void)
+{
+	printf("%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	exit(EXIT_FAILURE);
+}
