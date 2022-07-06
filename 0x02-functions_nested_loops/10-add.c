c7c10c7374f4a8b4c305a64073fb325e5b797329
Salami Ifeoluwa Mistura <omosholapesalami@gmail.com>
2022-07-06 16:09:56 -0700

    Addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - addition of integers
+ * @a: variable bank
+ * @b: variable bank
+ * Return: Always 0.
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
