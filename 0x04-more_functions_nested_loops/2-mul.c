ec8667626353d5400e1e46bff20823e4797bfd44
Salami Ifeoluwa Mistura <omosholapesalami@gmail.com>
2022-07-07 13:26:32 -0700

    multiply two var

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - multiply two int
+ * @a: var bank
+ * @b: var bank
+ * Return: Result of the multiplied var
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
