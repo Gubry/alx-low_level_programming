64ea2026e80fbeccaeaaa7b4bd63b09ee3a5d1dc
Salami Ifeoluwa Mistura <omosholapesalami@gmail.com>
2022-07-07 02:17:21 -0700

    Checks for +ve and -Ve int

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - checks for positive or negative number
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * positive_or_negative - checks for +ve or -ve
+ * @i:var bank
+ * Return:ALways 0.
+ */
+
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	else
+	{
+		printf("%d is negative\n", i);
+	}
+}
