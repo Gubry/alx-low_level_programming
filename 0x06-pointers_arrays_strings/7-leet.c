534d30b0c9a7d0583d1648ef6fc4b3dfa0d0778d
Salami Ifeoluwa Mistura <omosholapesalami@gmail.com>
2022-07-14 15:42:55 -0700

    swap letters to numbers

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * leet - flip some letters to numbers
+ * @s: holds the string
+ * Return: the flipped string
+ */
+char *leet(char *s)
+{
+	int i;
+	int j;
+	char l[] = "ol_ea__t";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; l[j] != '\0'; j++)
+		{
+			if (s[i] == l[j] || s[i] == (l[j] - 32))
+			{
+				s[i] = j + '0';
+			}
+		}
+	}
+	return (s);
+}
